681a5b16eed6d85f2b69f41682af98dd824ee3d0
Igor <61516126+igoriilla-projects@users.noreply.github.com>
2020-02-27 17:16:50 +0500

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-SOURCE="/home/igor/files/"
-TARGET="/home/igor/backups"
+SOURCE="/path/to/source/files/"
+TARGET="/path/where/backup"
 LOGS="/var/log/sync"
 
 SENDMAIL=$(ssmtp -s recepient_mail@mail.com < mail.txt)
 
-FREESPACE=$(df -h | grep /dev/sda2 | awk '{ print $5 }' | cut -c 1)
+FREESPACE=$(df -h | grep MOUNT-POINT | awk '{ print $5 }' | cut -c 1)
 PANIC="90"
 
 TIMESTAMP=`date "+%Y-%m-%d"`
